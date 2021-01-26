da037c4fb6112706cf91cec4a92195114a8c7ee1
Joseph Quiroz <me@josephquiroz.com>
2021-01-26 10:28:49 -0500

    Editando el tag y simplificando la respuesta de la actividad principal

--- app/src/main/java/com/example/androidniubizintegration/MainActivity.kt
+++ app/src/main/java/com/example/androidniubizintegration/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
 import lib.visanet.com.pe.visanetlib.VisaNet
 import lib.visanet.com.pe.visanetlib.presentation.custom.VisaNetViewAuthorizationCustom
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "Niubiz"
+    private val TAG = "Payment"
     private lateinit var openFormButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         data[VisaNet.VISANET_CHANNEL] = VisaNet.Channel.MOBILE
         data[VisaNet.VISANET_COUNTABLE] = true
         data[VisaNet.VISANET_SECURITY_TOKEN] =
-            "eyJraWQiOiJmWk1tV3pZR0RBckxHektvalNCK2w3SjFhMnNPXC9zQnNwOTlNNmNuM3F5MD0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0NzFmYjk3Yy0xNjNlLTQyYjYtOGI3OC03Zjk1YjA1OGM1NTgiLCJldmVudF9pZCI6ImI3NjU4YzZlLWZlMmYtNDJjNC05YjZkLTZkZTFkNjhiZDczYyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTExNzQ2OTgsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xXzJjSjFTZTFmSSIsImV4cCI6MTYxMTE3ODI5OCwiaWF0IjoxNjExMTc0Njk4LCJqdGkiOiIyMzU3NWQ2Ny0wZTFhLTQzYmMtOTMzMy05YmQwMDNhMmZiZGQiLCJjbGllbnRfaWQiOiIxMGx2MDYxN281ZGljNTFlYnNucWVpaWpiNyIsInVzZXJuYW1lIjoiaW50ZWdyYWNpb25lc0BuaXViaXouY29tLnBlIn0.W8b-e-QgGVaYbvyNSiMTTcqDzU97xDFViwsYCatGq5zKpfXfB5FFm_2yuWZM0hKkIy5ofmAsc4HTHO_qpappnQFYVvF_f2Ti-jBnaD8XcQrxYEIZT2vfnLKM6vBQIzjUltbwIAQQG5iHrHQ0WNoLzSLVvcDyuL12xsB8GnmVVrDfwXDBZOJZ-pgkMEsbRDFNVGJu3CCoV3z6IHqf227hKkJ_MdoQaXVeKnsKw9_rsUQ-8MKz9KR2YhXWlmd7qjy2nWf6uqoR0qFiz9OTeuWSNon6xwq9Fy9rMT5CITE25om4W3axsjUGXF6SmWrtQ6yNg5hj6MMkYtL9USSLX6Zplw"
+            "eyJraWQiOiJmWk1tV3pZR0RBckxHektvalNCK2w3SjFhMnNPXC9zQnNwOTlNNmNuM3F5MD0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0NzFmYjk3Yy0xNjNlLTQyYjYtOGI3OC03Zjk1YjA1OGM1NTgiLCJldmVudF9pZCI6Ijg3YjRlNDliLWMxMDAtNDExNS05MGY0LThlNmU4MDlhOTQ2ZCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTE2NzQ1NjIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xXzJjSjFTZTFmSSIsImV4cCI6MTYxMTY3ODE2MiwiaWF0IjoxNjExNjc0NTYyLCJqdGkiOiJlYWE5YzY4Ny01MjRmLTQxYzQtYWQyZS0yNDEyZTVlZTBmMzUiLCJjbGllbnRfaWQiOiIxMGx2MDYxN281ZGljNTFlYnNucWVpaWpiNyIsInVzZXJuYW1lIjoiaW50ZWdyYWNpb25lc0BuaXViaXouY29tLnBlIn0.T_fu0ZrHZfZxMNECWCBa59PRONin0CXnKwcfaqnwvLUBsaWKIeK6lbT4yVB0ps0Ubo4o0JbD5XAMw5jmZNzAG3e6K_Qt1kRRzpTZmZlBNmwFPcmvTSwiiT_ODbLV3o8DwiNUoHJDQL7pQMuAoBwgc_w6-YLNcq1Zip5A1YMMxMoBPf6DKxuljcDuvhC_BRpD395nQ4pxWZO4XGlrPQ1Yl-w_ynEMuFKMkbsaYBLOroX67Ln-wtrjzNDnijPgNywctoAJjj1jarIiF3RXeyVyJCfZKRVJt6hH95S1dXSwYbjBSuuuP_FUszMwABQUD1LNOcabnFbyzkLcWA19ElxLvA"
         data[VisaNet.VISANET_MERCHANT] = "456879852"
         data[VisaNet.VISANET_PURCHASE_NUMBER] = "1790"
         data[VisaNet.VISANET_AMOUNT] = "15.22"
@@ -36,9 +35,10 @@ class MainActivity : AppCompatActivity() {
         data[VisaNet.VISANET_CERTIFICATE_HOST] = "apisandbox.vnforappstest.com"
         data[VisaNet.VISANET_CERTIFICATE_PIN] =
             "sha256/lmxiL6uol7hb4UwDxtk2qbF2lBnJc7zqZRT6sFfYWEE="
-        data[VisaNet.VISANET_REGISTER_NAME] = "Juan";
-        data[VisaNet.VISANET_REGISTER_LASTNAME] = "Perez";
-        data[VisaNet.VISANET_REGISTER_EMAIL] = "jperez@test.com";
+        data[VisaNet.VISANET_REGISTER_NAME] = "Juan"
+        data[VisaNet.VISANET_REGISTER_LASTNAME] = "Perez"
+        data[VisaNet.VISANET_REGISTER_EMAIL] = "jperez@test.com"
+//        data[VisaNet.VISANET_USER_TOKEN] = "demo@gmail.com"
 
         val custom = VisaNetViewAuthorizationCustom()
         custom.logoImage = R.drawable.tulogo
@@ -48,16 +48,16 @@ class MainActivity : AppCompatActivity() {
             VisaNet.authorization(this@MainActivity, data, custom)
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.i(TAG, "onClick: " + e.message)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == VisaNet.VISANET_AUTHORIZATION) {
-            // TODO: Implementar respuesta del formulario
-        } else {
-            Toast.makeText(applicationContext, "Unauthorized", Toast.LENGTH_LONG).show()
+        when (requestCode) {
+            VisaNet.VISANET_AUTHORIZATION -> {
+                // TODO: Implementar respuesta del formulario
+            }
+            else -> Log.i(TAG, "Request code not implemented")
         }
     }
 }
